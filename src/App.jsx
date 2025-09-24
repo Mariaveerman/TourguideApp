f7cfd1f421fe1d1f7a36c8f6a9a85922de724065
Mariaveerman <72742@hoornbeeck.nl>
2025-09-24 10:38:30 +0200

    app verandering

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,6 @@ import Login from "./components/Login";
 
 const App = () => {
   return (
-<<<<<<< HEAD
-    <div>
-      <Tourguide />
-      <a href="./Route.jsx">link text</a>
-=======
     <div style={{
       background: 'linear-gradient(135deg, #667eea 0%, #764ba2 100%)',
       height: '100vh',
@@ -15,7 +10,6 @@ const App = () => {
       position: 'relative'
     }}>
       <Login />
->>>>>>> 88300a51e134b142a88c6b673bb0922e7e503bec
     </div>
   );
 };
